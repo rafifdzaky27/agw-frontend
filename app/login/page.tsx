f4ac080932193dbb84095eed82bb7129ce566639
dzzlr <dazzlerind07@gmail.com>
2025-05-27 09:29:40 +0700

    Update: New Login Page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,11 +17,14 @@ export default function Home() {
     toast.loading("Logging in...");
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_IP}/api/auth/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_IP}/api/auth/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        }
+      );
 
       toast.dismiss();
 
@@ -46,56 +49,77 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-gray-900 to-gray-800">
-      <motion.h1
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-        className="text-4xl font-extrabold text-white mb-8"
-      >
-        Change Advisory Board
-      </motion.h1>
-      <motion.div
-        initial={{ opacity: 0, scale: 0.9 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-        className="p-8 bg-gray-800 bg-opacity-90 backdrop-blur-lg rounded-2xl shadow-xl max-w-sm w-full"
-      >
-        <h2 className="text-3xl font-bold text-center text-white mb-6">Welcome Back</h2>
-
-        <form onSubmit={handleLogin} className="space-y-5">
-          <div>
-            <label className="block text-sm font-medium text-gray-300">Username</label>
-            <input
-              type="text"
-              placeholder="Enter username"
-              className="w-full px-4 py-2 border rounded-lg bg-gray-700 text-white focus:ring focus:ring-blue-500"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium text-gray-300">Password</label>
-            <input
-              type="password"
-              placeholder="Enter password"
-              className="w-full px-4 py-2 border rounded-lg bg-gray-700 text-white focus:ring focus:ring-blue-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg transition-all duration-300 hover:bg-blue-700 shadow-lg"
+    <div className="h-screen w-full flex flex-col md:flex-row-reverse ">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/2 bg-blue-600 text-white">
+        <div className="flex flex-col gap-2 md:gap-4 w-full md:w-3/4 p-4 md:p-0 text-start">
+          <motion.h1
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-xl md:text-4xl font-extrabold"
           >
-            Login
-          </motion.button>
-        </form>
-      </motion.div>
+            Architecture & Governance Workspace
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-sm md:text-xl font-light"
+          >
+            The essential tool for managing daily operations such as project/RFC
+            registration, Change Advisory Board activities, and audit findings
+            monitoring. This platform is the go-to hub for optimizing governance
+            and ensuring smooth project execution with seamless coordination and
+            oversight.
+          </motion.p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center w-full md:w-1/2 py-5 md:py-0">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="w-full md:w-3/4 px-4 md:py-0"
+        >
+          <h2 className="text-3xl font-bold mb-6">Login</h2>
+          <form onSubmit={handleLogin} className="flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <label className="block font-medium">
+                Username
+              </label>
+              <input
+                type="text"
+                placeholder="Enter username"
+                className="w-full px-4 py-2 border rounded-lg bg-slate-200 focus:ring focus:ring-blue-500"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+
+            <div className="flex flex-col gap-2 mb-3">
+              <label className="block font-medium ">
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder="Enter password"
+                className="w-full px-4 py-2 border rounded-lg bg-slate-200 focus:ring focus:ring-blue-500"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2 rounded-lg transition-all duration-300 hover:bg-blue-700 shadow-lg"
+            >
+              Login
+            </motion.button>
+          </form>
+        </motion.div>
+      </div>
     </div>
   );
 }
